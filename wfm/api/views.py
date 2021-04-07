6b87100857dd0ec695019915967bf46628be72f7
valitovstas <valitovstas@yandex.ru>
2021-04-07 12:58:27 +0700

    API правила планирования

--- wfm/api/views.py
+++ wfm/api/views.py
@@ -208,9 +208,9 @@ class PlanningMethodView(generics.ListAPIView):
 
     def get_queryset(self):
         queryset = Planning_Method.objects.all()
-        id = self.request.query_params.get('id', None)
-        if id is not None:
-            queryset = queryset.filter(id=id)
+        self_id = self.request.query_params.get('id', None)
+        if self_id is not None:
+            queryset = queryset.filter(id=self_id)
         return queryset
 
 
